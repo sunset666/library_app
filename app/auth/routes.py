7c46136c40a16138a1e3dacc1da53b7e24808c3a
David Betancur <sunset666@bemovil.net>
2022-02-22 15:57:07 -0500

    bugfix removing attribute

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -20,7 +20,7 @@ def login():
         if user is None or not user.check_password(form.password.data) or not user.activated:
             flash('Invalid username or password or please use the link sent to your email!!')
             return redirect(url_for('auth.login'))
-        login_user(user, remember=form.remember_me.data)
+        login_user(user)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('main.index')
